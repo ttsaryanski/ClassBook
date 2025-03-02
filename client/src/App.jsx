93f09aa32e10b1c4f1afbb2c1ae8a01dacac5d4b
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-03-02 13:17:04 +0200

    Change folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 import Header from "./components/header/Header";
 import UserList from "./components/UserList";
-import Footer from "./components/Footer";
+import Footer from "./components/footer/Footer";
 
 function App() {
     return (
--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,7 +1,9 @@
+import styles from "./Footer.module.css";
+
 export default function Footer() {
     return (
-        <footer className="footer">
-            <p>
+        <footer className={styles.footer}>
+            <p className={styles.par}>
                 This site is designed to be used for training purposes at
                 SoftUni.
             </p>
