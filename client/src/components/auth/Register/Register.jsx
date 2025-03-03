fe7007473e5229d94bd6d3b8daade37f92896022
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-03-03 17:33:31 +0200

    Register form fix

--- client/src/components/auth/Register/Register.jsx
+++ client/src/components/auth/Register/Register.jsx
@@ -90,14 +90,6 @@ export default function Register() {
                                 >
                                     Password
                                 </label>
-                                {/* <div className="text-sm">
-                                    <a
-                                        href="#"
-                                        className="font-semibold text-indigo-600 hover:text-indigo-500"
-                                    >
-                                        Forgot password?
-                                    </a>
-                                </div> */}
                             </div>
                             <div className="mt-2">
                                 <input
@@ -106,6 +98,27 @@ export default function Register() {
                                     type="password"
                                     required
                                     autoComplete="current-password"
+                                    className={`${styles.input} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6`}
+                                />
+                            </div>
+                        </div>
+
+                        <div className={styles.form_row}>
+                            <div className="flex items-center justify-between">
+                                <label
+                                    htmlFor="rePassword"
+                                    className={`${styles.label} block text-sm/6 font-medium text-gray-900`}
+                                >
+                                    Repeat Password
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="rePassword"
+                                    name="rePassword"
+                                    type="password"
+                                    required
+                                    autoComplete="current-rePassword"
                                     className={`${styles.input} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6`}
                                 />
                             </div>
