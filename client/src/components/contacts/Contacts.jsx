3ac8ef3420c8c1e45ba0c726a3a18540d3ba9e4e
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-03-03 18:39:57 +0200

    Edit Contacts page

--- client/src/components/contacts/Contacts.jsx
+++ client/src/components/contacts/Contacts.jsx
@@ -11,40 +11,70 @@ export default function Contacts() {
                     <h2 className={styles.h2}>Contact Us</h2>
                 </div>
                 <div className={styles.row}>
-                    <div className={styles.col_md_6}>
-                        <form className={styles.form}>
-                            <div className={styles.input_container}>
-                                <input
-                                    className={styles.input}
-                                    type="text"
-                                    placeholder="Name"
-                                />
+                    <div
+                        className={`${styles.col_md_6} col-sm-12 col-md-12 col-lg-4`}
+                    >
+                        <div
+                            className={`${styles.contact_info} contact-info white`}
+                        >
+                            <div className="contact-item media">
+                                {" "}
+                                <i className="fa fa-map-marker-alt media-left media-right-margin"></i>
+                                <div
+                                    className={`${styles.media_body} media-body`}
+                                >
+                                    <p className="text-uppercase">Address</p>
+                                    <p className="text-uppercase">
+                                        City, Country
+                                    </p>
+                                </div>
                             </div>
-                            <div className={styles.input_container}>
-                                <input
-                                    className={styles.input}
-                                    type="text"
-                                    placeholder="Phone Number"
-                                />
+                            <div className="contact-item media">
+                                {" "}
+                                <i className="fa fa-mobile media-left media-right-margin"></i>
+                                <div
+                                    className={`${styles.media_body} media-body`}
+                                >
+                                    <p className="text-uppercase">Phone</p>
+                                    <p className="text-uppercase">
+                                        <a className="" href="tel:+15173977100">
+                                            (359) 000 00 00 00
+                                        </a>{" "}
+                                    </p>
+                                </div>
                             </div>
-                            <div className={styles.input_container}>
-                                <input
-                                    className={styles.input}
-                                    type="email"
-                                    placeholder="Email"
-                                />
+                            <div className="contact-item media">
+                                {" "}
+                                <i className="fa fa-envelope media-left media-right-margin"></i>
+                                <div
+                                    className={`${styles.media_body} media-body`}
+                                >
+                                    <p className="text-uppercase">E-mail</p>
+                                    <p className="text-uppercase">
+                                        <a
+                                            className=""
+                                            href="mailto:abcdefg@gmail.com"
+                                        >
+                                            email@gmail.com
+                                        </a>{" "}
+                                    </p>
+                                </div>
                             </div>
-                            <div className={styles.input_container}>
-                                <input
-                                    className={`${styles.input} ${styles.message_box}`}
-                                    type="text"
-                                    placeholder="Message"
-                                />
+                            <div className="contact-item media">
+                                {" "}
+                                <i className="fa fa-clock media-left media-right-margin"></i>
+                                <div
+                                    className={`${styles.media_body} media-body`}
+                                >
+                                    <p className="text-uppercase">
+                                        Working Hours
+                                    </p>
+                                    <p className="text-uppercase">
+                                        Mon-Fri 9.00H to 17.00H.
+                                    </p>
+                                </div>
                             </div>
-                            <div className={styles.d_flex}>
-                                <button className={styles.button}>SEND</button>
-                            </div>
-                        </form>
+                        </div>
                     </div>
                     <div className={styles.col_md_6}>
                         <div className={styles.map_container}>
@@ -58,6 +88,42 @@ export default function Contacts() {
                             </div>
                         </div>
                     </div>
+                </div>
+
+                <div className={`${styles.col_md_6} ${styles.contacts_form}`}>
+                    <form className={styles.form}>
+                        <div className={styles.input_container}>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                placeholder="Name"
+                            />
+                        </div>
+                        <div className={styles.input_container}>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                placeholder="Phone Number"
+                            />
+                        </div>
+                        <div className={styles.input_container}>
+                            <input
+                                className={styles.input}
+                                type="email"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div className={styles.input_container}>
+                            <input
+                                className={`${styles.input} ${styles.message_box}`}
+                                type="text"
+                                placeholder="Message"
+                            />
+                        </div>
+                        <div className={styles.d_flex}>
+                            <button className={styles.button}>SEND</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </section>
